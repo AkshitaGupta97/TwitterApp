f579fc9224ba02f50604ce913ea44372cc55b85a
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-11-10 20:47:58 +0530

    changes in index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,35 @@ import express from 'express';
 // create new app server
 const app = express();
 
-app.get('/ping', (req, res) => {
+// middleware to parse json request body
+
+function mid1(req,res,next){
+    console.log("this is my first middleware", next);
+    next();
+}
+function mid2(req,res,next){
+    console.log("this is my second middleware", next);
+    next();
+}
+function mid3(req,res,next){
+    console.log("this is my third middleware");
+    next();
+}
+
+const middleware = [mid1, mid2, mid3]
+
+// mid1 is a middleware function that logs a message for every request.
+// next is a function that passes control to the next middleware in the stack.
+app.get('/ping', middleware,  (req, res) => {
     return res.json({
         message: "hello pong"
     })
 }); 
+
 // create a route, in postman we can test it by hitting localhost:3000/ping, and get response 'hello pong'.
 // we are using json format to send the response.
 
-app.post('/tweets', (req, res) => {
+app.post('/tweets', middleware , (req, res) => {
     return res.json({
         tweets: [
             {
